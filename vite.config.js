f2d6213bf7b99a705c04b1635cce2423cdf367d3
nvandrie <nvandrie2@huskers.unl.edu>
2026-04-26 22:08:25 -0500

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nvandrie.github.io/algo-cheatsheet/",
+  base: "/algo-cheatsheet/",
 });
